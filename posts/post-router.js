480077bc5ae19ac2100edacb8eba614072f03dab
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-07 16:31:14 -0400

    got the update working correctly for post

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req,res) => {
 db.find()
 .then(posts => res.status(200).json(posts))
-.catch(err => res.status(500).json({error: `an error occured when trying to retrieve the posts for the database: ${err}`}))
+.catch(err => res.status(500).json({error: "The posts information could not be retrieved."}))
 })
 
 router.post('/', (req,res)=> {
@@ -54,8 +54,30 @@ router.delete('/:id', (req,res)=>{
         }).catch(err => res.status(500).json({error: "The post could not be removed"}))
     })
     .catch(err => {
-        res.status(500).json({message: "The post with the specified ID does not exist."})
+        res.status(500).json({message: "error while trying to find the id in database."})
     })
+})
+
+router.put('/:id',(req, res)=>{
+    const id = req.params.id;
+    const body = req.body
+    if(body.title && body.contents){
+        db.update(id, body)
+        .then(numberOfUpdates => {
+            console.log(numberOfUpdates)
+            if(numberOfUpdates > 0 ){
+                db.findById(id)
+                .then( object => {
+                    res.status(200).json(object)
+                }).catch(err => res.status(500).json({message:'the post was updated but the updated post couldnt be returned'}) )
+            }else{
+                res.status(404).json({error: 'cannot update any post in the database with the given id '})
+            }
+        })
+        .catch(err => res.status(500).json({error: "The post information could not be modified."}))
+    }else{
+        return res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+    }
 })
 
 module.exports = router;
