7a1989bdd1492cfee81c40837ec649cfb9b8942c
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-07 15:03:37 -0400

    finished the non :id endpoint for the server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./api/server');
 const port = 5005
+
 server.listen( port, ()=> {
     console.log(`listening on port ${port}`)
 })
--- posts/post-router.js
+++ posts/post-router.js
@@ -1,10 +1,28 @@
 const express = require('express')
-const Hubs = require('../data/db');
+const db = require('../data/db');
 const router = express.Router();
 
 
-router.get('/', (req,res)=>{
-    res.status(200).json({message: 'the router is working correctly'})
+router.get('/', (req,res) => {
+db.find()
+.then(posts => res.status(200).json(posts)
+.catch(err => res.status(500).json({error: `an error occured when trying to retrieve the posts for the database: ${err}`})))
+})
+
+router.post('/', (req,res)=> {
+    const post = req.body;
+    if(post.title && post.contents){
+        return db.insert(post)
+        .then(postObject => {
+            db.findById(postObject.id )
+            .then(postNeeded => res.status(201).json(postNeeded)
+            )
+            .catch(err => res.status(500).json({message: 'couldnt retrieved the updated post'}))
+        })
+        .catch( err => res.status(500).json({message: `There was an error while saving the post to the database`}))
+    }else{
+        return res.status(404).json({message:'Please provide title and contents for the post.'})
+    }
 })
 
 module.exports = router;
